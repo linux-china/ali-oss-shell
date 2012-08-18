7fd93cebf3965a8e0f1a65057502edef5613461b
Jacky Chan <libing.chen@gmail.com>
2012-08-18 16:13:14 +0800

    config service implementation

---
+++ src/main/java/org/mvnsearch/ali/oss/spring/services/impl/ConfigServiceImpl.java
@@ -0,0 +1,95 @@
+package org.mvnsearch.ali.oss.spring.services.impl;
+
+import org.mvnsearch.ali.oss.spring.services.ConfigService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+/**
+ * config service implementation
+ *
+ * @author linux_china
+ */
+@Component("configService")
+public class ConfigServiceImpl implements ConfigService {
+    /**
+     * global properties
+     */
+    private Properties properties;
+
+    /**
+     * post construct
+     */
+    @PostConstruct
+    public void init() {
+        try {
+            properties = new Properties();
+            File cfgFile = new File(new File(System.getProperty("user.home")), ".aliyunoss.cfg");
+            if (cfgFile.exists()) {
+                properties.load(new FileInputStream(cfgFile));
+            }
+        } catch (Exception ignore) {
+
+        }
+    }
+
+    /**
+     * available info
+     *
+     * @return available infor
+     */
+    @Override
+    public boolean available() {
+        return properties.containsKey("ACCESS_ID") && properties.contains("ACCESS_KEY");
+    }
+
+    /**
+     * get configuration
+     *
+     * @param key key
+     * @return value
+     */
+    @Override
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    /**
+     * update config
+     *
+     * @param key   key
+     * @param value value
+     */
+    @Override
+    public void setProperty(String key, String value) {
+        try {
+            File cfgFile = new File(new File(System.getProperty("user.home")), ".aliyunoss.cfg");
+            properties.setProperty(key, value);
+            properties.store(new FileOutputStream(cfgFile), null);
+        } catch (Exception e) {
+
+        }
+    }
+
+    /**
+     * set access info
+     *
+     * @param accessId  access id
+     * @param accessKey access key
+     */
+    public void setAccessInfo(String accessId, String accessKey) {
+        try {
+            properties.setProperty("ACCESS_ID", accessId);
+            properties.setProperty("ACCESS_KEY", accessKey);
+            File cfgFile = new File(new File(System.getProperty("user.home")), ".aliyunoss.cfg");
+            properties.store(new FileOutputStream(cfgFile), null);
+        } catch (Exception ignore) {
+
+        }
+    }
+
+}
