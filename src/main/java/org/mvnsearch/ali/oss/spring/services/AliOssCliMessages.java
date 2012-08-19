9974de233486545fc3bda9a422be5a7deeb94f15
Jacky Chan <libing.chen@gmail.com>
2012-08-19 21:04:41 +0800

    aliyun oss CLI messages

---
+++ src/main/java/org/mvnsearch/ali/oss/spring/services/AliOssCliMessages.java
@@ -0,0 +1,53 @@
+package org.mvnsearch.ali.oss.spring.services;
+
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.PropertyKey;
+
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * aliyun oss CLI messages
+ *
+ * @author linux_china
+ */
+public class AliOssCliMessages {
+    /**
+     * bundle FQN
+     */
+    @NonNls
+    private static final String BUNDLE_FQN = "i18n.Messages";
+
+    /**
+     * resource bundle
+     */
+    private static ResourceBundle ourBundle = ResourceBundle.getBundle(BUNDLE_FQN);
+
+    /**
+     * 私有构造函数，禁止进行创建对象操作
+     */
+    private AliOssCliMessages() {
+    }
+
+    /**
+     * 获取resource bundle中对应的bundle值
+     *
+     * @param key    bundle key
+     * @param params 参数，bundle的值采用MessageFormat的格式化方式
+     * @return bundle值，如果bundle key不存在，返回特定key丢失格式
+     */
+    public static String message(@PropertyKey(resourceBundle = BUNDLE_FQN) String key, Object... params) {
+        String value;
+        try {
+            value = ourBundle.getString(key);
+        } catch (MissingResourceException ignore) {
+            value = "!!!" + key + "!!!";
+        }
+        if (params.length > 0 && value.indexOf('{') >= 0) {
+            value = MessageFormat.format(value, params);
+        }
+        return value;
+    }
+
+}
