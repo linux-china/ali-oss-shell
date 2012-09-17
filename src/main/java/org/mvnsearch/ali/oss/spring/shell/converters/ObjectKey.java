60f2912ffced18764062b543d580110e79d0c4c6
Jacky Chan <libing.chen@gmail.com>
2012-09-17 22:53:08 +0800

    object key

---
+++ src/main/java/org/mvnsearch/ali/oss/spring/shell/converters/ObjectKey.java
@@ -0,0 +1,29 @@
+package org.mvnsearch.ali.oss.spring.shell.converters;
+
+/**
+ * object key
+ *
+ * @author linux_china
+ */
+public class ObjectKey {
+    /**
+     * key
+     */
+    private String key;
+
+    public ObjectKey() {
+
+    }
+
+    public ObjectKey(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
