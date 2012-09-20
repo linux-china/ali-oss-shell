4b03e17665277b607ce52990d073d3d3405863a8
Jacky Chan <libing.chen@gmail.com>
2012-09-20 12:13:17 +0800

    search manager implementation test

---
+++ src/test/java/org/mvnsearch/ali/oss/spring/services/impl/SearchManagerImplTest.java
@@ -0,0 +1,39 @@
+package org.mvnsearch.ali.oss.spring.services.impl;
+
+import junit.framework.TestCase;
+import org.mvnsearch.ali.oss.spring.services.OssObjectDocument;
+import org.mvnsearch.ali.oss.spring.services.SearchManager;
+
+import java.util.Date;
+
+/**
+ * search manager implementation test
+ *
+ * @author linux_china
+ */
+public class SearchManagerImplTest extends TestCase {
+    /**
+     * search manager
+     */
+    private SearchManager searchManager;
+
+    /**
+     * Sets up the fixture, for example, open a network connection.
+     * This method is called before a test is executed.
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        searchManager = new SearchManagerImpl();
+    }
+
+    public void testIndex() throws Exception {
+        OssObjectDocument document = new OssObjectDocument();
+        document.setBucket("linux_china");
+        document.setPath("cms");
+        document.setName("demo.png");
+        document.setContentType("image/png");
+        document.setContentLength(130000);
+        document.setDate(new Date());
+        searchManager.index(document);
+    }
+}
