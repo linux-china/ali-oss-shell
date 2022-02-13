96e0208043c7cee5425e7859a9cfd07a03998490
linux_china <libing.chen@gmail.com>
2022-02-12 22:32:11 -0800

    OssShellApp

---
+++ src/main/java/org/mvnsearch/ali/oss/spring/OssShellApp.java
@@ -0,0 +1,11 @@
+package org.mvnsearch.ali.oss.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssShellApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OssShellApp.class, args);
+    }
+}
