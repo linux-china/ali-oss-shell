7444699efa06d507be21393501e889deb7366025
linux_china <libing.chen@gmail.com>
2022-02-12 22:21:24 -0800

    Update to Spring Shell 2.1.0

--- src/main/java/org/mvnsearch/ali/oss/spring/shell/converters/BucketEnumConverter.java
+++ src/main/java/org/mvnsearch/ali/oss/spring/shell/converters/BucketEnumConverter.java
@@ -1,8 +1,13 @@
 package org.mvnsearch.ali.oss.spring.shell.converters;
 
-import org.springframework.shell.core.Completion;
-import org.springframework.shell.core.Converter;
-import org.springframework.shell.core.MethodTarget;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.shell.CompletionContext;
+import org.springframework.shell.CompletionProposal;
+import org.springframework.shell.standard.ValueProvider;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -11,70 +16,25 @@ import java.util.List;
  *
  * @author linux_china
  */
-public class BucketEnumConverter implements Converter<BucketEnum> {
-    /**
-     * Indicates whether this converter supports the given type in the given option context
-     *
-     * @param type          the type being checked
-     * @param optionContext a non-<code>null</code> string that customises the
-     *                      behaviour of this converter for a given {@link org.springframework.shell.core.annotation.CliOption} of a given
-     *                      {@link org.springframework.shell.core.annotation.CliCommand}; the contents will have special meaning to this
-     *                      converter (e.g. be a comma-separated list of keywords known to this
-     *                      converter)
-     * @return see above
-     */
-    public boolean supports(Class<?> type, String optionContext) {
-        return type.getCanonicalName().equals(BucketEnum.class.getCanonicalName());
+@Component
+public class BucketEnumConverter implements Converter<String, BucketEnum>, ValueProvider {
+
+
+    @Override
+    public BucketEnum convert(@NotNull String value) {
+        BucketEnum bucket = new BucketEnum();
+        bucket.setName(value);
+        return bucket;
+
     }
 
-    /**
-     * Converts from the given String value to type T
-     *
-     * @param value         the value to convert
-     * @param targetType    the type being converted to; can't be <code>null</code>
-     * @param optionContext a non-<code>null</code> string that customises the
-     *                      behaviour of this converter for a given {@link org.springframework.shell.core.annotation.CliOption} of a given
-     *                      {@link org.springframework.shell.core.annotation.CliCommand}; the contents will have special meaning to this
-     *                      converter (e.g. be a comma-separated list of keywords known to this
-     *                      converter)
-     * @return see above
-     * @throws RuntimeException if the given value could not be converted
-     */
-    @SuppressWarnings("unchecked")
-    public BucketEnum convertFromText(String value, Class<?> targetType, String optionContext) {
-        Class<BucketEnum> enumClass = (Class<BucketEnum>) targetType;
-        try {
-            BucketEnum bucket = enumClass.newInstance();
-            bucket.setName(value);
-            return bucket;
-        } catch (Exception ignore) {
-
-        }
-        return null;
+    @Override
+    public boolean supports(MethodParameter parameter, CompletionContext completionContext) {
+        return parameter.getParameterType() == BucketEnum.class;
     }
 
-    /**
-     * Populates the given list with the possible completions
-     *
-     * @param completions   the list to populate; can't be <code>null</code>
-     * @param targetType    the type of parameter for which a string is being entered
-     * @param existingData  what the user has typed so far
-     * @param optionContext a non-<code>null</code> string that customises the
-     *                      behaviour of this converter for a given {@link org.springframework.shell.core.annotation.CliOption} of a given
-     *                      {@link org.springframework.shell.core.annotation.CliCommand}; the contents will have special meaning to this
-     *                      converter (e.g. be a comma-separated list of keywords known to this
-     *                      converter)
-     * @param target
-     * @return <code>true</code> if all the added completions are complete
-     *         values, or <code>false</code> if the user can press TAB to add further
-     *         information to some or all of them
-     */
-    public boolean getAllPossibleValues(List<Completion> completions, Class<?> targetType, String existingData, String optionContext, MethodTarget target) {
-        for (String candidate : BucketEnum.getBucketNames()) {
-            if (existingData==null || "".equals(existingData) || candidate.startsWith(existingData) || existingData.startsWith(candidate) || candidate.toUpperCase().startsWith(existingData.toUpperCase()) || existingData.toUpperCase().startsWith(candidate.toUpperCase())) {
-                completions.add(new Completion(candidate));
-            }
-        }
-        return true;
+    @Override
+    public List<CompletionProposal> complete(MethodParameter parameter, CompletionContext completionContext, String[] hints) {
+        return BucketEnum.getBucketNames().stream().map(CompletionProposal::new).toList();
     }
 }
--- src/main/java/org/mvnsearch/ali/oss/spring/shell/converters/ObjectKeyConverter.java
+++ src/main/java/org/mvnsearch/ali/oss/spring/shell/converters/ObjectKeyConverter.java
@@ -1,14 +1,20 @@
 package org.mvnsearch.ali.oss.spring.shell.converters;
 
-import com.aliyun.openservices.oss.model.OSSObjectSummary;
-import com.aliyun.openservices.oss.model.ObjectListing;
+import com.aliyun.oss.model.OSSObjectSummary;
+import com.aliyun.oss.model.ObjectListing;
+import org.jetbrains.annotations.NotNull;
 import org.mvnsearch.ali.oss.spring.services.AliyunOssService;
 import org.mvnsearch.ali.oss.spring.services.OSSUri;
 import org.mvnsearch.ali.oss.spring.shell.commands.OssOperationCommands;
-import org.springframework.shell.core.Completion;
-import org.springframework.shell.core.Converter;
-import org.springframework.shell.core.MethodTarget;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.shell.CompletionContext;
+import org.springframework.shell.CompletionProposal;
+import org.springframework.shell.standard.ValueProvider;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -16,7 +22,8 @@ import java.util.List;
  *
  * @author linux_china
  */
-public class ObjectKeyConverter implements Converter<ObjectKey> {
+@Component
+public class ObjectKeyConverter implements Converter<String, ObjectKey>, ValueProvider {
     /**
      * aliyun OSS service
      */
@@ -31,84 +38,41 @@ public class ObjectKeyConverter implements Converter<ObjectKey> {
         this.aliyunOssService = aliyunOssService;
     }
 
-    /**
-     * Indicates whether this converter supports the given type in the given option context
-     *
-     * @param type          the type being checked
-     * @param optionContext a non-<code>null</code> string that customises the
-     *                      behaviour of this converter for a given {@link org.springframework.shell.core.annotation.CliOption} of a given
-     *                      {@link org.springframework.shell.core.annotation.CliCommand}; the contents will have special meaning to this
-     *                      converter (e.g. be a comma-separated list of keywords known to this
-     *                      converter)
-     * @return see above
-     */
-    public boolean supports(Class<?> type, String optionContext) {
-        return type.getCanonicalName().equals(ObjectKey.class.getCanonicalName());
+    @Override
+    public ObjectKey convert(@NotNull String source) {
+        return new ObjectKey(source);
     }
 
-    /**
-     * Converts from the given String value to type T
-     *
-     * @param value         the value to convert
-     * @param targetType    the type being converted to; can't be <code>null</code>
-     * @param optionContext a non-<code>null</code> string that customises the
-     *                      behaviour of this converter for a given {@link org.springframework.shell.core.annotation.CliOption} of a given
-     *                      {@link org.springframework.shell.core.annotation.CliCommand}; the contents will have special meaning to this
-     *                      converter (e.g. be a comma-separated list of keywords known to this
-     *                      converter)
-     * @return see above
-     * @throws RuntimeException if the given value could not be converted
-     */
-    @SuppressWarnings("unchecked")
-    public ObjectKey convertFromText(String value, Class<?> targetType, String optionContext) {
-        Class<ObjectKey> enumClass = (Class<ObjectKey>) targetType;
-        try {
-            ObjectKey bucket = enumClass.newInstance();
-            bucket.setKey(value);
-            return bucket;
-        } catch (Exception ignore) {
-
-        }
-        return null;
+    @Override
+    public boolean supports(MethodParameter parameter, CompletionContext completionContext) {
+        return parameter.getParameterType() == ObjectKey.class;
     }
 
-    /**
-     * Populates the given list with the possible completions
-     *
-     * @param completions   the list to populate; can't be <code>null</code>
-     * @param targetType    the type of parameter for which a string is being entered
-     * @param existingData  what the user has typed so far
-     * @param optionContext a non-<code>null</code> string that customises the
-     *                      behaviour of this converter for a given {@link org.springframework.shell.core.annotation.CliOption} of a given
-     *                      {@link org.springframework.shell.core.annotation.CliCommand}; the contents will have special meaning to this
-     *                      converter (e.g. be a comma-separated list of keywords known to this
-     *                      converter)
-     * @param target target method
-     * @return <code>true</code> if all the added completions are complete
-     *         values, or <code>false</code> if the user can press TAB to add further
-     *         information to some or all of them
-     */
-    public boolean getAllPossibleValues(List<Completion> completions, Class<?> targetType, String existingData, String optionContext, MethodTarget target) {
+    @Override
+    public List<CompletionProposal> complete(MethodParameter parameter, CompletionContext completionContext, String[] hints) {
+        String existingData = completionContext.currentWord();
         OSSUri objectUri = OssOperationCommands.currentBucket;
         if (existingData != null && existingData.startsWith("oss://")) {
-            return true;
+            return Collections.emptyList();
         }
         try {
             String bucketName = objectUri.getBucket();
             String key = objectUri.getFilePath();
             ObjectListing objectListing = aliyunOssService.list(bucketName, key + existingData, 40);
+            List<CompletionProposal> completions = new ArrayList<>();
             for (OSSObjectSummary objectSummary : objectListing.getObjectSummaries()) {
                 String candidate = objectSummary.getKey();
                 if (key != null && candidate.startsWith(key)) {
                     candidate = candidate.replace(key, "");
                 }
                 if (existingData == null || "".equals(existingData) || candidate.startsWith(existingData) || existingData.startsWith(candidate) || candidate.toUpperCase().startsWith(existingData.toUpperCase()) || existingData.toUpperCase().startsWith(candidate.toUpperCase())) {
-                    completions.add(new Completion(candidate));
+                    completions.add(new CompletionProposal(candidate));
                 }
             }
+            return completions;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return true;
+        return Collections.emptyList();
     }
 }
